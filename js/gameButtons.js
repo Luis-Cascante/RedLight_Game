a33044fb27e89299e9d4e0d45af70ed4e909900c
Luis-Cascante <luisalonsocascante@gmail.com>
2025-11-14 18:04:11 -0600

    buttons and cauldron

--- js/gameButtons.js
+++ js/gameButtons.js
@@ -41,14 +41,12 @@ selectTool(tool){
 template: /*html*/`
 
  <div class="buttons-container">
-                <div class="buttons-panel">
-                    <button v-on:click="$emit('selectTool','resume'); $emit('selected-view','game-panel')"><img src="./img/ResumeBtn.png" alt="resume"></button>
-                    <button v-on:click="$emit('selected-view', 'cauldron-panel')"><img src="./img/potions.png" alt="potions"></button>
-                    <button v-on:click="$emit('selected-view', 'inventory-panel')"><img src="./img/inventory-btn.png" alt="inventory"></button>
-                    <button v-on:click="$emit('selected-view', 'store-panel')"><img src="./img/store.png" alt="store"></button>
-                    <button v-on:click="$emit('selected-view', 'main-menu')"><img src="./img/exit-btn.png" alt="exit"></button>
-                </div>
-            </div>
+    <button v-on:click="$emit('selectTool','resume'); $emit('selected-view','game-panel')"><img src="./img/ResumeBtn.png" alt="resume"></button>
+    <button v-on:click="$emit('selected-view', 'cauldron-panel')"><img src="./img/potions.png" alt="potions"></button>
+    <button v-on:click="$emit('selected-view', 'inventory-panel')"><img src="./img/inventory-btn.png" alt="inventory"></button>
+    <button v-on:click="$emit('selected-view', 'store-panel')"><img src="./img/store.png" alt="store"></button>
+    <button v-on:click="$emit('selected-view', 'main-menu')"><img src="./img/exit-btn.png" alt="exit"></button>
+</div>
 `
     
 });
